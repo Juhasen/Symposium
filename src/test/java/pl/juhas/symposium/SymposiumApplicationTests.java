96fc34138a68cfbc2e19d901fbb93f486f0ccb75
Juhasen <juszczyk-krystian@wp.pl>
2025-04-29 19:45:21 +0200

    Configuring tests

--- src/test/java/pl/juhas/symposium/SymposiumApplicationTests.java
+++ src/test/java/pl/juhas/symposium/SymposiumApplicationTests.java
@@ -1,13 +1,46 @@
 package pl.juhas.symposium;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import pl.juhas.symposium.repository.ConferenceHallRepository;
+import pl.juhas.symposium.repository.ParticipantRepository;
+import pl.juhas.symposium.repository.PresentationRepository;
+import pl.juhas.symposium.repository.TopicRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@Transactional
+@Rollback
 @SpringBootTest
 class SymposiumApplicationTests {
 
+    @Autowired
+    private ConferenceHallRepository conferenceHallRepository;
+
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    @Autowired
+    private PresentationRepository presentationRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+
     @Test
     void contextLoads() {
+        assertThat(conferenceHallRepository).isNotNull();
+        assertThat(participantRepository).isNotNull();
+        assertThat(presentationRepository).isNotNull();
+        assertThat(topicRepository).isNotNull();
+        log.info("All repositories are loaded successfully.");
     }
+
+
 
 }
