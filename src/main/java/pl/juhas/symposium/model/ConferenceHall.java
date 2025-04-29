9369fffb58f07a2113149e04a272618a133fadd0
Juhasen <juszczyk-krystian@wp.pl>
2025-04-29 18:17:40 +0200

    Hotel and ConferenceHall models

---
+++ src/main/java/pl/juhas/symposium/model/ConferenceHall.java
@@ -0,0 +1,27 @@
+﻿package pl.juhas.symposium.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "conference_hall")
+public class ConferenceHall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+}
---
+++ src/main/java/pl/juhas/symposium/model/Hotel.java
@@ -0,0 +1,32 @@
+﻿package pl.juhas.symposium.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+import java.util.List;
+
+@Data
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ConferenceHall> conferenceHalls;
+
+
+}
